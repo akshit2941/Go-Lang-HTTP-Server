26267f97e822aaa9af5edb3b402c632b79660301
akshit2941 <sainiakshit2941@gmail.com>
2025-06-21 00:43:04 +0530

    basic handler implemented

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main(){
-	
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
